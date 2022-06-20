966d7db4457979adca3277edc391dacde7812051
cspark0610 <cpark0610@gmail.com>
2022-06-20 14:45:23 -0300

    downgraded tailwind version to 2.1.4

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,16 @@
 module.exports = {
+	plugins: ["tailwindcss", "postcss-nesting", "autoprefixer"],
+};
+
+/*
+FOR TAILWIND VEERSION 3
 	plugins: {
 		"postcss-import": {},
 		"tailwindcss/nesting": "postcss-nesting",
 		tailwindcss: {},
 		autoprefixer: {},
 	},
-};
+*/
 
 // "postcss-nesting"
 // to apply & > * { styles }
